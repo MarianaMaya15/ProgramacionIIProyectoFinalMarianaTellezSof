bd0a4a472eb8e319b69c75472eefe6276ca40647
MarianaMaya15 <mariana.rodriguezm@uqvirtual.edu.co>
2026-05-21 19:36:27 -0500

    Agregar enumeraciones EstadoEvento, EstadoIncidencia y EstadoPago para gestionar los estados de eventos, incidencias y pagos en la aplicación

---
+++ src/main/java/co/uniquindio/eventos/model/enums/EstadoIncidencia.java
@@ -0,0 +1,8 @@
+package co.edu.uniquindio.eventos.model.enums;
+
+public enum EstadoIncidencia {
+    ABIERTA,
+    EN_PROCESO,
+    RESUELTA,
+    CERRADA
+}
---
+++ src/main/java/co/uniquindio/eventos/model/enums/EstadoPago.java
@@ -0,0 +1,8 @@
+package co.edu.uniquindio.eventos.model.enums;
+
+public enum EstadoPago {
+    PENDIENTE,
+    APROBADO,
+    RECHAZADO,
+    REEMBOLSADO
+}
